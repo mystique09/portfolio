6f6915e08b499829fd18367fa485e68d42efdda8
mystique09 <knightkrypto7@gmail.com>
2021-06-13 16:40:27 +0800

    added responsiveness

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
+
+const menu = document.querySelector<HTMLDivElement>('.menu')!;
+const mobileNav = document.querySelector<HTMLDivElement>('.nav-mobile-modal')!;
+const closeNavModal = document.querySelector<HTMLDivElement>('.nav-mobile-modal > .close')!;
+
+menu.onclick = function(e: any){
+  mobileNav.classList.remove('hidden');
+}
+
+closeNavModal.onclick = function(e: any){
+  mobileNav.classList.add('hidden');
+}
